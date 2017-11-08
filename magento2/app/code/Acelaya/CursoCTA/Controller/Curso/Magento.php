3050302c7bc5460b3f4394e349b60fce461e730b
Alejandro Celaya <alejandro@alejandrocelaya.com>
2017-11-08 20:37:17 +0100

    Created first module

---
+++ magento2/app/code/Acelaya/CursoCTA/Controller/Curso/Magento.php
@@ -0,0 +1,22 @@
+<?php
+namespace Acelaya\CursoCTA\Controller\Curso;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\ResponseInterface;
+
+class Magento extends Action
+{
+    /**
+     * Execute action based on request and return result
+     *
+     * Note: Request will be added as operation argument in future
+     *
+     * @return \Magento\Framework\Controller\ResultInterface|ResponseInterface
+     * @throws \Magento\Framework\Exception\NotFoundException
+     */
+    public function execute()
+    {
+        echo '<h1>vaya cantidad de mierda hay que crear pa ná</h1>';
+        die();
+    }
+}
